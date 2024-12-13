96c5d5234e2a73968e6dc9db9d02a1bf09ba6b4b
FrankGitMaster <frank_psn@outlook.com>
2024-12-13 11:43:35 -0500

    Prueba de commit con git bash

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo good 5',
+      title: 'Todo good 6',
       theme: ThemeData(
         // This is the theme of your application.
         //
