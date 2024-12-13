c3d0dfc1ed62d55d0071c087364c6c76760a4f6f
FrankGitMaster <frank_psn@outlook.com>
2024-12-13 11:33:03 -0500

    Se cambia la ubicacion de la carpeta como prueba

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo good 4',
+      title: 'Todo good 5',
       theme: ThemeData(
         // This is the theme of your application.
         //
